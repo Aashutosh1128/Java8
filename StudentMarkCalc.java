653f0325bd8cc77974366079124a0b5c36f7c062
Aashu1128 <vermaaashutosh34@gmail.com>
2025-06-22 08:23:52 +0530

    add data

--- StudentMarkCalc.java
+++ StudentMarkCalc.java
@@ -1,3 +1,17 @@
 public class StudentMarkCalc {
+    int rollNumber;
+    String stuName;
+    int mathsM;
+    int phyM;
+    int chemM;
+
+    StudentMarkCalc(int rollNumber,String stuName,int mathsM,int phyM,int chemM){
+        this.rollNumber=rollNumber;
+        this.stuName=stuName;
+        this.mathsM=mathsM;
+        this.phyM=phyM;
+        this.chemM=chemM;
+
+    }
 
 }
